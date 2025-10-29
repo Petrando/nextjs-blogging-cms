d743e96ffc4e313aa9f8411650fbb4928bac702c
Petrando <petrandorichard@gmail.com>
2025-10-29 13:03:38 +0800

    popular-post-skeleton component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
 import { MainNav } from "@/components/main-nav";
 import LatestPosts from "@/components/home/latest-posts";
+import Container from "@/components/Container";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <Container>
       <MainNav />
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+      <main className="flex flex-col items-start justify-evenly mt-16 md:flex-row">
+        <div>
         <LatestPosts />
+        </div>
       </main>
-    </div>
+    </Container>
   );
 }
