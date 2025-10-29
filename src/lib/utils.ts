30b6d74f9c60a51676d5903c18b98ca87d0694b0
Petrando <petrandorichard@gmail.com>
2025-10-29 13:06:36 +0800

    popular-posts component

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,18 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const fetchUrl =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000/api"
+    : "https://nextjs-blogging-cms.vercel.app/api";
+
+type ResponseData = {
+  category: string;
+  title: string;
+  slug: string;
+}[];
+
+export const fetcher = (
+  ...args: Parameters<typeof fetch>
+): Promise<ResponseData> => fetch(...args).then((res) => res.json());
