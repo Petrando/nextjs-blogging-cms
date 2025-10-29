805be09c2fd8a343cb8d0f470400b98bc08d6d46
Petrando <petrandorichard@gmail.com>
2025-10-29 14:29:02 +0800

    Breadcrumb, Header, ReportViews and mdx components

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -14,7 +14,7 @@ export default function Footer() {
   return (
     <footer className="bg-gray-100 py-8 dark:bg-gray-800 mt-10">
       <div className="container mx-auto px-4 md:px-6">
-        <div className="grid grid-cols-1 gap-8 md:grid-cols-4">
+        <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-4">
           <div className="space-y-4">
             <div className="flex items-center space-x-2">
               <Icons.logo className="h-6 w-6" />
